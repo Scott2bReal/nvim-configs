3e7138d2deab3508332d10b41ccc09b0c093ca75
Scott Hoecker <hoeckers@gmail.com>
2022-05-10 21:59:55 -0500

    Add sqlfluff

--- lua/scott/lsp/null-ls.lua
+++ lua/scott/lsp/null-ls.lua
@@ -12,10 +12,11 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
   debug = false,
   sources = {
-    formatting.prettier.with { extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } },
-    formatting.black.with { extra_args = { "fast" } },
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "fast" } }),
     formatting.stylua,
     formatting.rubocop,
+    formatting.sqlfluff.with({ extra_args = { "--dialect", "postgres"}}),
     diagnostics.flake8,
-  }
+  },
 })
