2870c12b4d3bfd38a32aff6a37710edeb157cc7c
Scott Hoecker <hoeckers@gmail.com>
2022-06-29 16:50:37 -0500

    Ignore .git dir in telescope

--- lua/scott/telescope.lua
+++ lua/scott/telescope.lua
@@ -15,6 +15,8 @@ telescope.setup {
     selection_caret = " ",
     path_display = { "smart" },
 
+    file_ignore_patterns = { ".git/", },
+
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
