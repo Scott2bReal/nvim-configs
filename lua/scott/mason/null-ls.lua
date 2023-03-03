2ebf0e050785061cc80968aa146741948e450792
Scott Hoecker <hoeckers@gmail.com>
2023-03-02 19:34:17 -0600

    Turn off extra args for prettier

--- lua/scott/mason/null-ls.lua
+++ lua/scott/mason/null-ls.lua
@@ -13,7 +13,7 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({
-			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+			-- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
 			extra_filetypes = { "astro" },
 		}),
 		formatting.black.with({ extra_args = { "fast" } }),
