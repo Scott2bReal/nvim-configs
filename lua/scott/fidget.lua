b065510584ce408b65476a7ea3e60330bf5ad5bb
Scott Hoecker <hoeckers@gmail.com>
2023-03-21 12:16:10 -0500

    Add rule to prettier, change fidget loading icon

--- lua/scott/fidget.lua
+++ lua/scott/fidget.lua
@@ -1,7 +1,50 @@
 local status_ok, fidget = pcall(require, "fidget")
 if not status_ok then
-  vim.notify("Fidget.nvim couldn't load")
-  return
+	vim.notify("Fidget.nvim couldn't load")
+	return
 end
 
-fidget.setup{}
+-- fidget.setup({})
+fidget.setup({
+	text = {
+		spinner = "bouncing_ball", -- animation shown when tasks are ongoing
+		done = "✔", -- character shown when all tasks are complete
+		commenced = "Started", -- message shown when task starts
+		completed = "Completed", -- message shown when task completes
+	},
+})
+
+-- The text.spinner option recognizes the following spinner pattern names:
+-- dots
+-- dots_negative
+-- dots_snake
+-- dots_footsteps
+-- dots_hop
+-- line
+-- pipe
+-- dots_ellipsis
+-- dots_scrolling
+-- star
+-- flip
+-- hamburger
+-- grow_vertical
+-- grow_horizontal
+-- noise
+-- dots_bounce
+-- triangle
+-- arc
+-- circle
+-- square_corners
+-- circle_quarters
+-- circle_halves
+-- dots_toggle
+-- box_toggle
+-- arrow
+-- zip
+-- bouncing_bar
+-- bouncing_ball
+-- clock
+-- earth
+-- moon
+-- dots_pulse
+-- meter
--- lua/scott/mason/null-ls.lua
+++ lua/scott/mason/null-ls.lua
@@ -14,6 +14,7 @@ null_ls.setup({
 	sources = {
 		formatting.prettier.with({
 			-- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+      extra_args = { "--bracket-same-line", },
 			extra_filetypes = { "astro" },
 		}),
 		formatting.black.with({ extra_args = { "fast" } }),
