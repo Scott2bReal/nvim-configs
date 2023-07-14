6e9abb3563f773e8c910e289680e8e63d00540d3
Scott Hoecker <hoeckers@gmail.com>
2023-07-13 20:49:47 -0500

    Set up markdown previewer

--- lua/scott/plugins.lua
+++ lua/scott/plugins.lua
@@ -24,9 +24,9 @@ local plugins = {
 	{ "nvim-lua/popup.nvim", lazy = true }, -- An implementation of the Popup API from vim
 	{ "nvim-lua/plenary.nvim", lazy = true }, -- Lua functions required by many plugins
 	{ "kyazdani42/nvim-web-devicons", lazy = true }, -- Icons required by many plugins
-	{ "famiu/bufdelete.nvim", lazy = true }, -- Makes sure BDelete won't exit neovim
+	{ "famiu/bufdelete.nvim", lazy = false }, -- Makes sure BDelete won't exit neovim
 	{ "antoinemadec/FixCursorHold.nvim", lazy = true }, -- This is needed to fix lsp doc highlight
-	{ "arithran/vim-delete-hidden-buffers", lazy = true }, -- Close all buffers but current
+	{ "arithran/vim-delete-hidden-buffers", lazy = false }, -- Close all buffers but current
 	{ "MunifTanjim/nui.nvim", lazy = true }, -- UI Plugin
 
 	-- Alpha
@@ -112,9 +112,9 @@ local plugins = {
 		"iamcco/markdown-preview.nvim",
 		build = "cd app && npm install",
 		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
+			vim.g.mkdp_filetypes = { "markdown", "vimwiki" }
 		end,
-		ft = { "markdown" },
+		ft = { "markdown", "vimwiki" },
 	},
 
 	-- NeoTree
@@ -168,6 +168,6 @@ local plugins = {
 	"folke/which-key.nvim", -- Keybind help popup
 }
 
-local opts = {}
+local opts = { browser = "firefox" }
 
 lazy.setup(plugins, opts)
