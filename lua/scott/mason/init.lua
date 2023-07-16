158c980ca586785ae6f24ad76f711929755bc4d1
Scott Hoecker <hoeckers@gmail.com>
2023-07-16 13:21:29 -0500

    Remove old configs

--- lua/scott/mason/init.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  vim.notify("Error loading Mason")
-	return
-end
-
-require("scott.mason.configs")
-require("scott.mason.handlers").setup()
-require("scott.mason.null-ls")
--- lua/scott/rust-tools.lua
+++
@@ -1,7 +0,0 @@
-local status_ok, rust_tools = pcall(require, "rust-tools")
-if not status_ok then
-  vim.notify("Error loading rust-tools")
-  return
-end
-
-rust_tools.setup()
