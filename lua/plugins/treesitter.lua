6d97e7e43dbe2ac2ed35750b5dd155c3b1362f15
Scott Hoecker <hoeckers@gmail.com>
2023-07-18 11:09:58 -0500

    Add new rainbow delimiter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,35 @@
 return {
-	{ "mrjones2014/nvim-ts-rainbow", lazy = true }, -- Color codes closure pairs like () or {}
+	{
+		"HiPhish/rainbow-delimiters.nvim",
+		event = { "BufReadPost", "BufNewFile" },
+		config = function()
+			local has_rainbow, rainbow = pcall(require, "rainbow-delimiters")
+			if not has_rainbow then
+				vim.notify("Failed to load rainbow delimiters")
+				return
+			end
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow.strategy["global"],
+					vim = rainbow.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+					tsx = "rainbow-parens",
+				},
+				highlight = {
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterCyan",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterRed",
+					"RainbowDelimiterBlue",
+				},
+			}
+		end,
+	},
 	{
 		"nvim-treesitter/nvim-treesitter", -- Robust syntax highlighting
 		build = ":TSUpdate",
