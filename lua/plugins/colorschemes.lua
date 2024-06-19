d0ac295423eebe186b14ed690d602dfdb3a06f90
Scott Hoecker <scott.hoecker-ctr@joinhandshake.com>
2024-06-19 09:09:19 -0500

    Add default options to new gruvbox scheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -4,7 +4,26 @@ return {
 		name = "gruvbox-material",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		opts = {
+			italics = true, -- enable italics in general
+			contrast = "medium", -- set contrast, can be any of "hard", "medium", "soft"
+			comments = {
+				italics = true, -- enable italic comments
+			},
+			background = {
+				transparent = false, -- set the background to transparent
+			},
+			float = {
+				force_background = false, -- force background on floats even when background.transparent is set
+				background_color = nil, -- set color for float backgrounds. If nil, uses the default color set
+				-- by the color scheme
+			},
+			signs = {
+				highlight = true, -- whether to highlight signs
+			},
+			customize = nil, -- customize the theme in any way you desire, see below what this
+			-- configuration accepts
+		},
 	},
 	{ "ellisonleao/gruvbox.nvim", lazy = true }, -- Gruvbox colorscheme in Lua
 	{
