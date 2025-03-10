243bc1844e5f74ed5da42471bca12b827a9c9070
Scott Hoecker <hoeckers@gmail.com>
2025-03-10 09:31:49 -0500

    Remove oil and update plugins

--- lua/plugins/markdown_preview.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"iamcco/markdown-preview.nvim",
-	build = "cd app && yarn install",
-	-- init = function()
-	-- 	vim.g.mkdp_filetypes = { "markdown", "vimwiki" }
-	-- end,
-	ft = { "markdown", "vimwiki" },
-}
