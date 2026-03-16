83cff3de4fef102a086be1bb206b4b48d69520f9
Scott Hoecker <hoeckers@gmail.com>
2026-03-15 20:02:16 -0500

    Redo mini config

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,28 +1,34 @@
+local plugins = {
+  files = {
+    windows = {
+      preview = true,
+      width_preview = 75,
+    }
+  },
+  pick = {
+    mappings = {
+      move_down  = '<C-j>',
+      move_up    = '<C-k>',
+      move_start = '<C-g>',
+    },
+    options = {
+      content_from_bottom = true
+    }
+  },
+  icons = {},
+  pairs = {},
+  tabline = {},
+  bufremove = {},
+  comment = {},
+  surround = {}
+}
+
 return {
   "nvim-mini/mini.nvim",
-  config = function() 
-    require("mini.icons").setup()
-    require("mini.pick").setup({
-      mappings = {
-        move_down  = '<C-j>',
-        move_up    = '<C-k>',
-        move_start = '<C-g>',
-      },
-      options = {
-        content_from_bottom = true
-      }
-    })
-    require("mini.files").setup({
-      windows = {
-        preview = true,
-        width_preview = 75,
-      }
-    })
-    require("mini.pairs").setup()
-    require("mini.git").setup()
-    require("mini.tabline").setup()
-    require("mini.bufremove").setup()
-    require("mini.comment").setup()
-    require("mini.extra").setup()
+  event = "VeryLazy",
+  config = function()
+    for name, opts in pairs(plugins) do
+      require("mini." .. name).setup(opts)
+    end
   end
 }
