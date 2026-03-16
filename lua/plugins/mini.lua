9a955120f33aa674d78d2849e6a7f912b733b060
Scott Hoecker <hoeckers@gmail.com>
2026-03-15 21:35:04 -0500

    Enable indent scope

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,35 +1,41 @@
 local plugins = {
-  files = {
-    windows = {
-      preview = true,
-      width_preview = 75,
+	files = {
+		windows = {
+			preview = true,
+			width_preview = 75,
+		},
+	},
+	pick = {
+		mappings = {
+			move_down = "<C-j>",
+			move_up = "<C-k>",
+			move_start = "<C-g>",
+		},
+		options = {
+			content_from_bottom = true,
+		},
+	},
+	icons = {},
+	pairs = {},
+	tabline = {
+		tabpage_section = "right",
+	},
+	bufremove = {},
+	comment = {},
+	indentscope = {
+		predicate = false,
+    draw = {
+      delay = 30
     }
-  },
-  pick = {
-    mappings = {
-      move_down  = "<C-j>",
-      move_up    = '<C-k>',
-      move_start = '<C-g>',
-    },
-    options = {
-      content_from_bottom = true
-    }
-  },
-  icons = {},
-  pairs = {},
-  tabline = {
-    tabpage_section = "right"
-  },
-  bufremove = {},
-  comment = {},
+	},
 }
 
 return {
-  "nvim-mini/mini.nvim",
-  event = "VeryLazy",
-  config = function()
-    for name, opts in pairs(plugins) do
-      require("mini." .. name).setup(opts)
-    end
-  end
+	"nvim-mini/mini.nvim",
+	event = "VeryLazy",
+	config = function()
+		for name, opts in pairs(plugins) do
+			require("mini." .. name).setup(opts)
+		end
+	end,
 }
