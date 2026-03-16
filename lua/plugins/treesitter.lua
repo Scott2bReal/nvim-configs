78aefefd5803473d1087562bcd1fd2d26216a985
Scott Hoecker <hoeckers@gmail.com>
2026-03-16 18:07:58 -0500

    Remove redundant plugin declaration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,37 +1,4 @@
-local highlights = {
-  "RainbowDelimiterYellow",
-  "RainbowDelimiterCyan",
-  "RainbowDelimiterOrange",
-  "RainbowDelimiterGreen",
-  "RainbowDelimiterViolet",
-  "RainbowDelimiterRed",
-  "RainbowDelimiterBlue",
-}
-
 return {
-  {
-    "HiPhish/rainbow-delimiters.nvim",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      local has_rainbow, rainbow = pcall(require, "rainbow-delimiters")
-      if not has_rainbow then
-        vim.notify("Failed to load rainbow delimiters")
-        return
-      end
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [""] = rainbow.strategy["global"],
-          vim = rainbow.strategy["local"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          lua = "rainbow-blocks",
-          tsx = "rainbow-parens",
-        },
-        highlight = highlights,
-      }
-    end,
-  },
   {
     "nvim-treesitter/nvim-treesitter", -- Robust syntax highlighting
     build = ":TSUpdate",
