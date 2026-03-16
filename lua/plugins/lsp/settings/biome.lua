0f33fccd1ccff9beab31e195348821ede9cbdff1
Scott Hoecker <hoeckers@gmail.com>
2026-03-15 21:04:08 -0500

    Add custom settings for biome

--- lua/plugins/lsp/settings/biome.lua
+++ lua/plugins/lsp/settings/biome.lua
@@ -1,20 +1,20 @@
 local ok, util = pcall(require, "lspconfig.util")
 if not ok then
-  vim.notify("lspconfig.util couldn't load")
-  return {}
+	vim.notify("lspconfig.util couldn't load")
+	return {}
 end
 
 local config = {
-  root_dir = util.root_pattern("biome.json", "biome.jsonc"),
-  filetypes = {
-    "javascript",
-    "javascriptreact", 
-    "typescript",
-    "typescriptreact",
-    "json",
-    "jsonc",
-  },
-  single_file_support = false, -- Only enable when biome.json is present
+	root_dir = util.root_pattern("biome.json", "biome.jsonc"),
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"json",
+		"jsonc",
+	},
+	single_file_support = false, -- Only enable when biome.json is present
 }
 
 return config
