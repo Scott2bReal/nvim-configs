a7a49a9ac83af120d027a8d6f798860eb60e486b
Scott Hoecker <hoeckers@gmail.com>
2026-03-16 12:47:14 -0500

    Update tabline config (colors, icons)

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,41 +1,57 @@
+local add_modified_icon = function(buf_nr, label)
+  local modified_icon = "●"
+  local is_modified = vim.api.nvim_get_option_value("modified", {
+    buf = buf_nr,
+  })
+  if is_modified then
+    return MiniTabline.default_format(buf_nr, label) .. modified_icon .. " "
+  else
+    return MiniTabline.default_format(buf_nr, label)
+  end
+end
+
 local plugins = {
-	files = {
-		windows = {
-			preview = true,
-			width_preview = 75,
-		},
-	},
-	pick = {
-		mappings = {
-			move_down = "<C-j>",
-			move_up = "<C-k>",
-			move_start = "<C-g>",
-		},
-		options = {
-			content_from_bottom = true,
-		},
-	},
-	icons = {},
-	pairs = {},
-	tabline = {
-		tabpage_section = "right",
-	},
-	bufremove = {},
-	comment = {},
-	-- indentscope = {
-	-- 	predicate = false,
-	--    draw = {
-	--      delay = 30
-	--    }
-	-- },
+  files = {
+    windows = {
+      preview = true,
+      width_preview = 75,
+    },
+  },
+  pick = {
+    mappings = {
+      move_down = "<C-j>",
+      move_up = "<C-k>",
+      move_start = "<C-g>",
+    },
+    options = {
+      content_from_bottom = true,
+    },
+  },
+  icons = {},
+  pairs = {},
+  tabline = {
+    tabpage_section = "right",
+    format = add_modified_icon,
+  },
+  bufremove = {},
+  comment = {},
+  -- indentscope = {
+  -- 	predicate = false,
+  --    draw = {
+  --      delay = 30
+  --    }
+  -- },
 }
 
 return {
-	"nvim-mini/mini.nvim",
-	event = "VeryLazy",
-	config = function()
-		for name, opts in pairs(plugins) do
-			require("mini." .. name).setup(opts)
-		end
-	end,
+  "nvim-mini/mini.nvim",
+  event = "VeryLazy",
+  config = function()
+    local colors = require("gruvbox-material.colors").get(vim.o.background, "medium")
+    vim.api.nvim_set_hl(0, "MiniTablineCurrent", { bg = colors.green, fg = colors.bg1 })
+    vim.api.nvim_set_hl(0, "MiniTablineModifiedCurrent", { bg = colors.green, fg = colors.bg1 })
+    for name, opts in pairs(plugins) do
+      require("mini." .. name).setup(opts)
+    end
+  end,
 }
