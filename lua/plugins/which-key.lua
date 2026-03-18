ac981ce1ebe03c2b236bdaf54457a85461749899
Scott Hoecker <hoeckers@gmail.com>
2026-03-18 17:05:55 -0500

    Improve keymap setup

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -53,6 +53,10 @@ return {
 			return
 		end
 
+		--- Telescope builtin functions
+		local telescope = require("telescope.builtin")
+		local telescope_themes = require("telescope.themes")
+
 		-- local configs = {
 		-- 	mode = "n", -- NORMAL mode
 		-- 	prefix = "<leader>",
@@ -72,7 +76,7 @@ return {
 			},
 			{
 				"<leader>f",
-				require("telescope.builtin").find_files,
+				telescope.find_files,
 				desc = "Find files",
 				nowait = true,
 				remap = false,
@@ -80,7 +84,7 @@ return {
 			{
 				"<leader>F",
 				function()
-					require("telescope.builtin").live_grep(require("telescope.themes").get_ivy())
+					telescope.live_grep(telescope_themes.get_ivy())
 				end,
 				desc = "Find Text",
 				nowait = true,
@@ -124,89 +128,6 @@ return {
 				remap = false,
 			},
 			{
-				"<leader>g",
-				group = "Git",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gR",
-				"<cmd>lua require 'gitsigns'.reset_buffer()<cr>",
-				desc = "Reset Buffer",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gb",
-				"<cmd>Telescope git_branches<cr>",
-				desc = "Checkout branch",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gc",
-				"<cmd>Telescope git_commits<cr>",
-				desc = "Checkout commit",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gj",
-				"<cmd>lua require 'gitsigns'.next_hunk()<cr>",
-				desc = "Next Hunk",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gk",
-				"<cmd>lua require 'gitsigns'.prev_hunk()<cr>",
-				desc = "Prev Hunk",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gl",
-				"<cmd>lua require 'gitsigns'.blame_line()<cr>",
-				desc = "Blame",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>go",
-				"<cmd>Telescope git_status<cr>",
-				desc = "Open changed file",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gp",
-				"<cmd>lua require 'gitsigns'.preview_hunk()<cr>",
-				desc = "Preview Hunk",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gr",
-				"<cmd>lua require 'gitsigns'.reset_hunk()<cr>",
-				desc = "Reset Hunk",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gs",
-				"<cmd>lua require 'gitsigns'.stage_hunk()<cr>",
-				desc = "Stage Hunk",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>gu",
-				"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-				desc = "Undo Stage Hunk",
-				nowait = true,
-				remap = false,
-			},
-			{
 				"<leader>h",
 				"<cmd>nohlsearch<CR>",
 				desc = "No Highlight",
@@ -230,20 +151,6 @@ return {
 				"<leader>lR",
 				"<cmd>LspRestart<cr>",
 				desc = "Restart LSP",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>lS",
-				"<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-				desc = "Workspace Symbols",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>ld",
-				"<cmd>Telescope diagnostics<cr>",
-				desc = "Document Diagnostics",
 				nowait = true,
 				remap = false,
 			},
@@ -421,18 +328,16 @@ return {
 			{
 				"<leader>sb",
 				function()
-					require("telescope.builtin").buffers(
-						require("telescope.themes").get_dropdown({ previewer = false })
-					)
+					telescope.buffers(telescope_themes.get_dropdown({ previewer = false }))
 				end,
-				desc = "Buffers",
+				desc = "Search buffers",
 				nowait = true,
 				remap = false,
 			},
 			{
 				"<leader>sc",
 				function()
-					require("telescope.builtin").colorscheme(require("telescope.themes").get_dropdown())
+					telescope.colorscheme(telescope_themes.get_dropdown())
 				end,
 				desc = "Search colorschemes",
 				nowait = true,
@@ -441,7 +346,7 @@ return {
 			{
 				"<leader>sd",
 				function()
-					require("telescope.builtin").find_files({ hidden = true })
+					telescope.find_files({ hidden = true })
 				end,
 				desc = "Include Dotfiles",
 				nowait = true,
@@ -449,36 +354,47 @@ return {
 			},
 			{
 				"<leader>sh",
-				"<cmd>Telescope help_tags<cr>",
+				function()
+					telescope.help_tags(telescope_themes.get_ivy())
+				end,
 				desc = "Find Help",
 				nowait = true,
 				remap = false,
 			},
 			{
 				"<leader>sk",
-				"<cmd>Telescope keymaps<cr>",
+				function()
+					telescope.keymaps(telescope_themes.get_ivy())
+				end,
 				desc = "Keymaps",
 				nowait = true,
 				remap = false,
 			},
 			{
 				"<leader>sr",
-				"<cmd>Telescope oldfiles<cr>",
+				telescope.oldfiles,
 				desc = "Open Recent File",
 				nowait = true,
 				remap = false,
 			},
 			{
 				"<leader>sR",
-				"<cmd>Telescope registers<cr>",
+				telescope.registers,
 				desc = "Search Registers",
 				nowait = true,
 				remap = false,
 			},
 			{
 				"<leader>sD",
-				"<cmd>Telescope diagnostics<cr>",
+				telescope.diagnostics,
 				desc = "Search Diagnostics",
+				nowait = true,
+				remap = false,
+			},
+			{
+				"<leader>sS",
+				telescope.lsp_dynamic_workspace_symbols,
+				desc = "Workspace Symbols",
 				nowait = true,
 				remap = false,
 			},
@@ -492,13 +408,6 @@ return {
 				"<leader>zc",
 				"<cmd>ColorizerToggle<cr>",
 				desc = "Colorizer",
-				nowait = true,
-				remap = false,
-			},
-			{
-				"<leader>zg",
-				"<cmd>ChatGPT<cr>",
-				desc = "ChatGPT",
 				nowait = true,
 				remap = false,
 			},
