dfffb7481f0da7d0848770c0bc1b0e76fe2a519d
Scott Hoecker <hoeckers@gmail.com>
2026-03-18 16:45:02 -0500

    Format

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,51 +1,51 @@
 return {
-  {
-    "f4z3r/gruvbox-material.nvim",
-    name = "gruvbox-material",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      contrast = "medium", -- set contrast, can be any of "hard", "medium", "soft"
-      customize = function(group, options)
-        if (group == "DiffChange") then
-          options.bg = "#4a0e0e"
-        end
-        return options
-      end
-    },
-  },
-  { "ellisonleao/gruvbox.nvim", lazy = true }, -- Gruvbox colorscheme in Lua
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = true,
-    opts = {
+	{
+		"f4z3r/gruvbox-material.nvim",
+		name = "gruvbox-material",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			contrast = "medium", -- set contrast, can be any of "hard", "medium", "soft"
+			customize = function(group, options)
+				if group == "DiffChange" then
+					options.bg = "#4a0e0e"
+				end
+				return options
+			end,
+		},
+	},
+	{ "ellisonleao/gruvbox.nvim", lazy = true }, -- Gruvbox colorscheme in Lua
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = true,
+		opts = {
 
-      compile = false,  -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,   -- do not set background color
-      dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = {             -- add/modify theme and palette colors
-        palette = {},
-        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      -- overrides = function(colors) -- add/modify highlights
-      -- 	return {}
-      -- end,
-      theme = "lotus", -- Load "wave" theme when 'background' option is not set
-      background = {   -- map the value of 'background' option to a theme
-        dark = "wave", -- try "dragon" !
-        light = "lotus",
-      },
-    },
-    config = function()
-      vim.cmd([[colorscheme kanagawa]])
-    end,
-  },
-  { "catppuccin/nvim",          name = "catpuccin", lazy = true },
+			compile = false, -- enable compiling the colorscheme
+			undercurl = true, -- enable undercurls
+			commentStyle = { italic = true },
+			functionStyle = {},
+			keywordStyle = { italic = true },
+			statementStyle = { bold = true },
+			typeStyle = {},
+			transparent = false, -- do not set background color
+			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+			terminalColors = true, -- define vim.g.terminal_color_{0,17}
+			colors = { -- add/modify theme and palette colors
+				palette = {},
+				theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+			},
+			-- overrides = function(colors) -- add/modify highlights
+			-- 	return {}
+			-- end,
+			theme = "lotus", -- Load "wave" theme when 'background' option is not set
+			background = { -- map the value of 'background' option to a theme
+				dark = "wave", -- try "dragon" !
+				light = "lotus",
+			},
+		},
+		config = function()
+			vim.cmd([[colorscheme kanagawa]])
+		end,
+	},
+	{ "catppuccin/nvim", name = "catpuccin", lazy = true },
 }
