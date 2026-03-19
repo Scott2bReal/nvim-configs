7f81d492145123466783ea26062dd7f0e3f9cc75
Scott Hoecker <hoeckers@gmail.com>
2026-03-18 21:48:34 -0500

    Update from deprecated vim.loop to vim.uv

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,6 +1,6 @@
 -- Automatically install Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
 	vim.fn.system({
 		"git",
 		"clone",
