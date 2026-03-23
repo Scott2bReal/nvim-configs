777b578101d18cafa9cc7e465cfe097c9cab9d32
Scott Hoecker <hoeckers@gmail.com>
2026-03-23 17:56:14 -0500

    Fix treesitter for now...

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,17 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter", -- Robust syntax highlighting
+		-- This needs to be updated when we switch to neovim 0.12
+		branch = "master",
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
+		-- lazy = false,
 		opts = {
-			ensure_installed = "all",
+			-- ensure_installed = "all",
 			sync_install = false,
 			ignore_install = { "" },
 			highlight = {
 				enable = true,
-				disable = { "" },
 				additional_vim_regex_highlighting = true,
 			},
 			indent = { enable = true, disable = { "yaml", "ruby", "html", "css", "sql" } },
@@ -23,6 +25,14 @@ return {
 				enable_autocmd = false,
 			},
 		},
+		config = function(_, opts)
+			local has_treesitter, ts_configs = pcall(require, "nvim-treesitter.configs")
+			if not has_treesitter then
+				vim.notify("nvim-treesitter.configs not found!", vim.log.levels.ERROR)
+				return
+			end
+			ts_configs.setup(opts)
+		end,
 	},
 	-- {
 	-- 	"nvim-treesitter/nvim-treesitter-context", -- Show context around cursor
