9b4fd2a0147d325ecdf41c77bf2386ee2edc41ee
Scott Hoecker <hoeckers@gmail.com>
2026-03-23 21:24:09 -0500

    Update nvim-treesitter to use main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,47 +1,60 @@
 return {
 	{
-		"nvim-treesitter/nvim-treesitter", -- Robust syntax highlighting
-		-- This needs to be updated when we switch to neovim 0.12
-		branch = "master",
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
-		-- lazy = false,
-		opts = {
-			-- ensure_installed = "all",
-			sync_install = false,
-			ignore_install = { "" },
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = true,
-			},
-			indent = { enable = true, disable = { "yaml", "ruby", "html", "css", "sql" } },
-			rainbow = {
-				enable = true,
-				extended_mode = true,
-				max_file_lines = nil,
-			},
-			context_commentstring = {
-				enable = true,
-				enable_autocmd = false,
-			},
-		},
-		config = function(_, opts)
-			local has_treesitter, ts_configs = pcall(require, "nvim-treesitter.configs")
-			if not has_treesitter then
-				vim.notify("nvim-treesitter.configs not found!", vim.log.levels.ERROR)
-				return
+		config = function()
+			local treesitter = require("nvim-treesitter")
+			local parsers = {
+				"bash",
+				"css",
+				"diff",
+				"dockerfile",
+				"gitcommit",
+				"gitignore",
+				"html",
+				"http",
+				"javascript",
+				"jsdoc",
+				"json",
+				"json5",
+				"lua",
+				"make",
+				"markdown",
+				"regex",
+				"rst",
+				"rust",
+				"scss",
+				"ssh_config",
+				"sql",
+				"terraform",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			}
+
+			for _, parser in ipairs(parsers) do
+				treesitter.install(parser)
 			end
-			ts_configs.setup(opts)
+
+			local patterns = {}
+			for _, parser in ipairs(parsers) do
+				local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+				for _, pattern in ipairs(parser_patterns) do
+					table.insert(patterns, pattern)
+				end
+			end
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = patterns,
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
 		end,
 	},
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter-context", -- Show context around cursor
-	-- 	event = "VeryLazy",
-	-- 	opts = {},
-	-- 	config = function(_, opts)
-	-- 		local utils = require("utils")
-	-- 		vim.api.nvim_set_hl(0, "TreesitterContext", { bg = utils.colors.bg1 })
-	-- 		require("treesitter-context").setup(opts)
-	-- 	end,
-	-- },
 }
