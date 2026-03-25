6b87934def0c8c7fc7ea8a5efaeade982e90b290
Scott Hoecker <hoeckers@gmail.com>
2026-03-24 23:02:06 -0500

    Change color of lualine plugin

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -33,7 +33,7 @@ return {
 							return project_root_dirname .. " > " .. string.gsub(file_path, "/", " > ")
 						end
 					end,
-					color = { fg = utils.colors.yellow },
+					color = { fg = utils.colors.purple },
 					cond = function()
 						local ft = vim.bo.filetype
 						local disabled_filetypes = { "minifiles", "help", "fzf", "lazy" }
