fb82a1396c887b25a459111d59b712bd57e953f9
Scott Hoecker <hoeckers@gmail.com>
2026-04-01 15:01:22 -0500

    Use rose-pine-moon

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
 	group = vim.api.nvim_create_augroup("immediate_colorscheme", { clear = true }),
 	callback = function()
 		vim.cmd.packadd("rose-pine")
-		vim.cmd("colorscheme rose-pine")
+		vim.cmd("colorscheme rose-pine-moon")
 	end,
 })
 
