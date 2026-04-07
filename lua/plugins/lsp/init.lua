dfa1413502c0cd53c6467343d0bb06f2c3d59f86
Scott Hoecker <hoeckers@gmail.com>
2026-04-07 14:13:35 -0500

    Lazy load lsp setups

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,46 +1,57 @@
-local servers = {
-	"bashls",
-	"jsonls",
-	"lua_ls",
-	"html",
-	"eslint",
-	"yamlls",
-	"oxfmt",
-	"oxlint",
-}
+vim.api.nvim_create_autocmd("BufReadPre", {
+	group = vim.api.nvim_create_augroup("lsp-setup", { clear = true }),
+	callback = function()
+		local servers = {
+			"bashls",
+			"jsonls",
+			"lua_ls",
+			"html",
+			"eslint",
+			"yamlls",
+			"oxfmt",
+			"oxlint",
+		}
 
-require("mason").setup({
-	ui = {
-		check_outdated_packages_on_open = false,
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
+		require("mason").setup({
+			ui = {
+				check_outdated_packages_on_open = false,
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+		require("mason-lspconfig").setup({
+			ensure_installed = servers,
+		})
+		local handlers = require("plugins.lsp.handlers")
+
+		handlers.setup()
+
+		for _, server in pairs(servers) do
+			local server_opts = {
+				on_attach = handlers.on_attach,
+				capabilities = handlers.capabilities,
+			}
+
+			local has_custom_opts, server_custom_opts = pcall(require, "plugins.lsp.settings." .. server)
+			if has_custom_opts then
+				server_opts = vim.tbl_deep_extend("force", server_opts, server_custom_opts)
+				vim.lsp.config(server, server_opts)
+				vim.lsp.enable(server)
+			else
+				vim.lsp.config(server, server_opts)
+				vim.lsp.enable(server)
+			end
+		end
+	end,
 })
-require("mason-lspconfig").setup({
-	ensure_installed = servers,
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "lua",
+	group = vim.api.nvim_create_augroup("lazydev-setup", { clear = true }),
+	callback = function()
+		require("lazydev").setup()
+	end,
 })
-
-require("lazydev").setup()
-
-local handlers = require("plugins.lsp.handlers")
-handlers.setup()
-
-for _, server in pairs(servers) do
-	local server_opts = {
-		on_attach = handlers.on_attach,
-		capabilities = handlers.capabilities,
-	}
-
-	local has_custom_opts, server_custom_opts = pcall(require, "plugins.lsp.settings." .. server)
-	if has_custom_opts then
-		server_opts = vim.tbl_deep_extend("force", server_opts, server_custom_opts)
-		vim.lsp.config(server, server_opts)
-		vim.lsp.enable(server)
-	else
-		vim.lsp.config(server, server_opts)
-		vim.lsp.enable(server)
-	end
-end
