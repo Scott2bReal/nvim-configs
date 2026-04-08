aea30e5f2efa3ca671c30cc5f791aeff19e48c1e
Scott Hoecker <hoeckers@gmail.com>
2026-04-08 09:27:04 -0500

    Lazy setup fzf lua

--- lua/plugins/settings/fzf-lua.lua
+++ lua/plugins/settings/fzf-lua.lua
@@ -1,26 +1,31 @@
-require("fzf-lua").setup({
-	{ "telescope" },
-	defaults = {
-		file_icons = "mini",
-	},
-	files = {
-		cwd_prompt = false,
-	},
-	oldfiles = {
-		cwd_prompt = false,
-		include_current_session = true,
-	},
-	helptags = {
-		profile = "ivy",
-	},
-	live_grep = {
-		profile = "ivy",
-	},
-	---@diagnostic disable: missing-fields
-	previewers = {
-		builtin = {
-			syntax_limit_b = 100 * 1024, -- 100kb
-		},
-	},
-	---@diagnostic enable: missing-fields
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("_fzf-lua-setup", { clear = true }),
+	callback = function()
+		require("fzf-lua").setup({
+			{ "telescope" },
+			defaults = {
+				file_icons = "mini",
+			},
+			files = {
+				cwd_prompt = false,
+			},
+			oldfiles = {
+				cwd_prompt = false,
+				include_current_session = true,
+			},
+			helptags = {
+				profile = "ivy",
+			},
+			live_grep = {
+				profile = "ivy",
+			},
+			---@diagnostic disable: missing-fields
+			previewers = {
+				builtin = {
+					syntax_limit_b = 100 * 1024, -- 100kb
+				},
+			},
+			---@diagnostic enable: missing-fields
+		})
+	end,
 })
