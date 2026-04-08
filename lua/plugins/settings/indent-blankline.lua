bd7bf570e0bc3674789506d06bbf6a1521586f48
Scott Hoecker <hoeckers@gmail.com>
2026-04-08 09:29:42 -0500

    Lazy setup more plugins

--- lua/plugins/settings/indent-blankline.lua
+++ lua/plugins/settings/indent-blankline.lua
@@ -1,5 +1,10 @@
-require("ibl").setup({
-	scope = {
-		enabled = false,
-	},
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = vim.api.nvim_create_augroup("_ibl-setup", { clear = true }),
+	callback = function()
+		require("ibl").setup({
+			scope = {
+				enabled = false,
+			},
+		})
+	end,
 })
