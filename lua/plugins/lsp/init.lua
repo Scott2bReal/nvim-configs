dafb234a3c0f54ea89ece863a3a58225c6cdc30d
Scott Hoecker <hoeckers@gmail.com>
2026-04-08 00:54:13 -0500

    Lazy load LSP setup

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,31 +1,63 @@
-local servers = {
-	"astro",
-	"biome",
-	"bashls",
-	"jsonls",
-	"lua_ls",
-	"html",
-	"eslint",
-	"yamlls",
-	"tailwindcss",
-	"prismals",
-	"oxlint",
-}
+----------------
+-- LSP Setup --
+----------------
+vim.api.nvim_create_autocmd("BufReadPre", {
+	group = vim.api.nvim_create_augroup("lsp-setup", { clear = true }),
+	callback = function()
+		local servers = {
+			"astro",
+			"biome",
+			"bashls",
+			"jsonls",
+			"lua_ls",
+			"html",
+			"eslint",
+			"yamlls",
+			"tailwindcss",
+			"prismals",
+			"oxlint",
+		}
 
-require("mason").setup({
-	ui = {
-		check_outdated_packages_on_open = false,
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
-})
-require("mason-lspconfig").setup({
-	ensure_installed = servers,
+		require("mason").setup({
+			ui = {
+				check_outdated_packages_on_open = false,
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		require("mason-lspconfig").setup({
+			ensure_installed = servers,
+		})
+
+		local handlers = require("plugins.lsp.handlers")
+		handlers.setup()
+
+		for _, server in pairs(servers) do
+			local server_opts = {
+				on_attach = handlers.on_attach,
+				capabilities = handlers.capabilities,
+			}
+
+			local has_custom_opts, server_custom_opts = pcall(require, "plugins.lsp.settings." .. server)
+			if has_custom_opts then
+				server_opts = vim.tbl_deep_extend("force", server_opts, server_custom_opts)
+				vim.lsp.config(server, server_opts)
+				vim.lsp.enable(server)
+			else
+				vim.lsp.config(server, server_opts)
+				vim.lsp.enable(server)
+			end
+		end
+	end,
 })
 
+----------------------------
+-- Special Server Setups --
+----------------------------
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = "lua",
 	group = vim.api.nvim_create_augroup("lazydev-setup", { clear = true }),
@@ -42,23 +74,3 @@ vim.api.nvim_create_autocmd("FileType", {
 		require("typescript-tools").setup({})
 	end,
 })
-
-local handlers = require("plugins.lsp.handlers")
-handlers.setup()
-
-for _, server in pairs(servers) do
-	local server_opts = {
-		on_attach = handlers.on_attach,
-		capabilities = handlers.capabilities,
-	}
-
-	local has_custom_opts, server_custom_opts = pcall(require, "plugins.lsp.settings." .. server)
-	if has_custom_opts then
-		server_opts = vim.tbl_deep_extend("force", server_opts, server_custom_opts)
-		vim.lsp.config(server, server_opts)
-		vim.lsp.enable(server)
-	else
-		vim.lsp.config(server, server_opts)
-		vim.lsp.enable(server)
-	end
-end
