1d1d8b22b3c45d503869fa17834766e969944b61
Scott Hoecker <hoeckers@gmail.com>
2026-04-09 09:34:29 -0500

    Lazy load treesitter and IBL

--- lua/plugins/settings/indent-blankline.lua
+++ lua/plugins/settings/indent-blankline.lua
@@ -1,5 +1,11 @@
-require("ibl").setup({
-	scope = {
-		enabled = false,
-	},
+vim.api.nvim_create_autocmd("UIEnter", {
+	group = vim.api.nvim_create_augroup("IndentBlanklineSetup", { clear = true }),
+	once = true,
+	callback = function()
+		require("ibl").setup({
+			scope = {
+				enabled = false,
+			},
+		})
+	end,
 })
